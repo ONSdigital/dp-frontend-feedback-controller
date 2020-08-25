acb10c08e124d7d5cda4a0d0636e318632c4c840
Andre Urbani <dredizzle@live.co.uk>
2020-08-25 14:04:00 +0100

    added handler (feedback.go) and endpoints to routes.go

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/ONSdigital/dp-frontend-feedback-controller
 go 1.13
 
 require (
+	github.com/ONSdigital/dp-api-clients-go v1.1.0
+	github.com/ONSdigital/dp-frontend-models v1.1.0
 	github.com/ONSdigital/dp-healthcheck v1.0.5
 	github.com/ONSdigital/go-ns v0.0.0-20200511161740-afc39066ee62
 	github.com/ONSdigital/log.go v1.0.1
 	github.com/gorilla/mux v1.7.4
+	github.com/gorilla/schema v1.1.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/smartystreets/goconvey v1.6.4
 )
--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-    "context"
+	"context"
 
 	"github.com/ONSdigital/dp-frontend-feedback-controller/config"
 	"github.com/ONSdigital/dp-frontend-feedback-controller/handlers"
@@ -13,7 +13,10 @@ import (
 
 // Setup registers routes for the service
 func Setup(ctx context.Context, r *mux.Router, cfg *config.Config, hc health.HealthCheck) {
-    log.Event(ctx, "adding routes")
+	log.Event(ctx, "adding routes")
 	r.StrictSlash(true).Path("/health").HandlerFunc(hc.Handler)
-	r.StrictSlash(true).Path("/helloworld").Methods("GET").HandlerFunc(handlers.HelloWorld(*cfg))
+	r.StrictSlash(true).Path("/feedback").Methods("POST").HandlerFunc(handlers.AddFeedback(auth, mailAddr, cfg.FeedbackTo, cfg.FeedbackFrom, cfg.RendererURL, false))
+	r.StrictSlash(true).Path("/feedback/positive").Methods("POST").HandlerFunc(handlers.AddFeedback(auth, mailAddr, cfg.FeedbackTo, cfg.FeedbackFrom, cfg.RendererURL, false))
+	r.StrictSlash(true).Path("/feedback").Methods("GET").HandlerFunc(handlers.GetFeedback(cfg.RendererURL))
+	r.StrictSlash(true).Path("/feedback/thanks").Methods("GET").HandlerFunc(handlers.FeedbackThanks(cfg.RendererURL))
 }
