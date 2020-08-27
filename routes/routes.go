5527ed472515ef14939a4ee61fb7ff9f87390840
Andre Urbani <dredizzle@live.co.uk>
2020-08-27 11:51:58 +0100

    added missing authentication logic to routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"context"
+	"fmt"
+	"net/smtp"
 
 	"github.com/ONSdigital/dp-frontend-feedback-controller/config"
 	"github.com/ONSdigital/dp-frontend-feedback-controller/handlers"
@@ -13,10 +15,35 @@ import (
 
 // Setup registers routes for the service
 func Setup(ctx context.Context, r *mux.Router, cfg *config.Config, hc health.HealthCheck) {
+
+	auth := smtp.PlainAuth(
+		"",
+		cfg.MailUser,
+		cfg.MailPassword,
+		cfg.MailHost,
+	)
+
+	if cfg.MailHost == "localhost" {
+		auth = unencryptedAuth{auth}
+	}
+
+	mailAddr := fmt.Sprintf("%s:%s", cfg.MailHost, cfg.MailPort)
+
 	log.Event(ctx, "adding routes")
 	r.StrictSlash(true).Path("/health").HandlerFunc(hc.Handler)
 	r.StrictSlash(true).Path("/feedback").Methods("POST").HandlerFunc(handlers.AddFeedback(auth, mailAddr, cfg.FeedbackTo, cfg.FeedbackFrom, cfg.RendererURL, false))
 	r.StrictSlash(true).Path("/feedback/positive").Methods("POST").HandlerFunc(handlers.AddFeedback(auth, mailAddr, cfg.FeedbackTo, cfg.FeedbackFrom, cfg.RendererURL, false))
 	r.StrictSlash(true).Path("/feedback").Methods("GET").HandlerFunc(handlers.GetFeedback(cfg.RendererURL))
 	r.StrictSlash(true).Path("/feedback/thanks").Methods("GET").HandlerFunc(handlers.FeedbackThanks(cfg.RendererURL))
+
+}
+
+type unencryptedAuth struct {
+	smtp.Auth
+}
+
+func (a unencryptedAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+	s := *server
+	s.TLS = true
+	return a.Auth.Start(&s)
 }
