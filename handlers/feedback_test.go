941ae23d2d8e6cb2536579761a2a9af53c27a964
Andre Urbani <dredizzle@live.co.uk>
2020-09-09 15:27:06 +0100

    added additional tests for email validation and sender error

--- handlers/feedback_test.go
+++ handlers/feedback_test.go
@@ -159,6 +159,44 @@ func Test_addFeedback(t *testing.T) {
 		})
 	})
 
+	Convey("Given an error returned from the sender", t, func() {
+
+		req := httptest.NewRequest("GET", "http://localhost?description=whatever", nil)
+		w := httptest.NewRecorder()
+		isPositive := false
+		from := ""
+		to := ""
+
+		mockRenderer := &interfacestest.RendererMock{
+			DoFunc: func(path string, b []byte) ([]byte, error) {
+				return nil, nil
+			},
+		}
+
+		mockSender := &emailtest.SenderMock{
+			SendFunc: func(from string, to []string, msg []byte) error {
+				return errors.New("email is broken")
+			},
+		}
+
+		Convey("When addFeedback is called", func() {
+
+			addFeedback(w, req, isPositive, mockRenderer, mockSender, from, to)
+
+			Convey("Then the renderer is not called", func() {
+				So(len(mockRenderer.DoCalls()), ShouldEqual, 0)
+			})
+
+			Convey("Then the email sender is called", func() {
+				So(len(mockSender.SendCalls()), ShouldEqual, 1)
+			})
+
+			Convey("Then a 500 response is returned", func() {
+				So(w.Code, ShouldEqual, http.StatusInternalServerError)
+			})
+		})
+	})
+
 	Convey("Given a request with invalid form data", t, func() {
 
 		req := httptest.NewRequest("POST", "http://localhost?!@£$@$£%£$%^^&^&*", nil)
@@ -230,6 +268,95 @@ func Test_addFeedback(t *testing.T) {
 				So(mockRenderer.DoCalls()[0].Path, ShouldEqual, "feedback")
 				rendererRequest := string(mockRenderer.DoCalls()[0].B)
 				So(strings.Contains(rendererRequest, `"error_type":"purpose"`), ShouldBeTrue)
+			})
+
+			Convey("Then the email sender is called", func() {
+				So(len(mockSender.SendCalls()), ShouldEqual, 0)
+			})
+
+			Convey("Then a 200 response is returned", func() {
+				So(w.Code, ShouldEqual, http.StatusOK)
+			})
+		})
+	})
+
+	Convey("Given a request for feedback with an empty description value", t, func() {
+
+		req := httptest.NewRequest("POST", "http://localhost?service=dev", nil)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		w := httptest.NewRecorder()
+		isPositive := false
+		from := ""
+		to := ""
+
+		mockRenderer := &interfacestest.RendererMock{
+			DoFunc: func(path string, b []byte) ([]byte, error) {
+				return nil, nil
+			},
+		}
+
+		mockSender := &emailtest.SenderMock{
+			SendFunc: func(from string, to []string, msg []byte) error {
+				return nil
+			},
+		}
+
+		Convey("When addFeedback is called", func() {
+
+			addFeedback(w, req, isPositive, mockRenderer, mockSender, from, to)
+
+			Convey("Then the renderer is called to render the feedback page", func() {
+				So(len(mockRenderer.DoCalls()), ShouldEqual, 1)
+				So(mockRenderer.DoCalls()[0].Path, ShouldEqual, "feedback")
+				rendererRequest := string(mockRenderer.DoCalls()[0].B)
+				So(strings.Contains(rendererRequest, `"error_type":"description"`), ShouldBeTrue)
+			})
+
+			Convey("Then the email sender is called", func() {
+				So(len(mockSender.SendCalls()), ShouldEqual, 0)
+			})
+
+			Convey("Then a 200 response is returned", func() {
+				So(w.Code, ShouldEqual, http.StatusOK)
+			})
+		})
+	})
+
+	//email
+
+	Convey("Given a request for feedback with an invalid email address", t, func() {
+
+		body := strings.NewReader("email=hello&description=hfjkshk")
+		req := httptest.NewRequest("POST", "http://localhost?service=dev", body)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		w := httptest.NewRecorder()
+		isPositive := false
+		from := ""
+		to := ""
+
+		mockRenderer := &interfacestest.RendererMock{
+			DoFunc: func(path string, b []byte) ([]byte, error) {
+				return nil, nil
+			},
+		}
+
+		mockSender := &emailtest.SenderMock{
+			SendFunc: func(from string, to []string, msg []byte) error {
+				return nil
+			},
+		}
+
+		Convey("When addFeedback is called", func() {
+
+			addFeedback(w, req, isPositive, mockRenderer, mockSender, from, to)
+
+			Convey("Then the renderer is called to render the feedback page", func() {
+				So(len(mockRenderer.DoCalls()), ShouldEqual, 1)
+				So(mockRenderer.DoCalls()[0].Path, ShouldEqual, "feedback")
+				rendererRequest := string(mockRenderer.DoCalls()[0].B)
+				So(strings.Contains(rendererRequest, `"error_type":"email"`), ShouldBeTrue)
 			})
 
 			Convey("Then the email sender is called", func() {
