6fb0c1b4307b819b37da1db7c5965b14d9118e84
Andre Urbani <dredizzle@live.co.uk>
2020-10-15 09:04:39 +0100

    removed additional instances of helloworld

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,11 +1,9 @@
 package handlers
 
 import (
-	"github.com/ONSdigital/dp-frontend-feedback-controller/config"
-	"github.com/ONSdigital/dp-frontend-feedback-controller/mapper"
-	"encoding/json"
-	"github.com/ONSdigital/log.go/log"
 	"net/http"
+
+	"github.com/ONSdigital/log.go/log"
 )
 
 // ClientError is an interface that can be used to retrieve the status code if a client has errored
@@ -23,31 +21,4 @@ func setStatusCode(req *http.Request, w http.ResponseWriter, err error) {
 	}
 	log.Event(req.Context(), "setting-response-status", log.Error(err))
 	w.WriteHeader(status)
-}
-
-// HelloWorld Handler
-func HelloWorld(cfg config.Config) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		helloWorld(w, req, cfg)
-	}
-}
-
-func helloWorld(w http.ResponseWriter, req *http.Request, cfg config.Config) {
-	ctx := req.Context()
-	greetingsModel := mapper.HelloModel{Greeting: "Hello", Who: "World"}
-	m := mapper.HelloWorld(ctx, greetingsModel, cfg)
-
-	b, err := json.Marshal(m)
-	if err != nil {
-		setStatusCode(req, w, err)
-		return
-	}
-
-	_, err = w.Write(b)
-    	if err != nil {
-    		log.Event(ctx, "failed to write bytes for http response", log.Error(err))
-    		setStatusCode(req, w, err)
-    		return
-    	}
-    	return
 }
