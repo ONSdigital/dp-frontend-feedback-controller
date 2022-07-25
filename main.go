f5fd64c8ff9693bd460eb3cb212d8fca3e873dce
Jon Dewi Jones <jondewijones@gmail.com>
2022-07-25 16:30:50 +0100

    init renderer library

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	health "github.com/ONSdigital/dp-healthcheck/healthcheck"
+	render "github.com/ONSdigital/dp-renderer"
 
 	"github.com/ONSdigital/dp-frontend-feedback-controller/config"
 	"github.com/ONSdigital/dp-frontend-feedback-controller/routes"
@@ -47,7 +48,10 @@ func main() {
 	if err = registerCheckers(ctx, &healthcheck); err != nil {
 		os.Exit(1)
 	}
-	routes.Setup(ctx, r, cfg, healthcheck)
+
+	rend := render.NewWithDefaultClient(assets.Asset, assets.AssetNames, cfg.PatternLibraryAssetsPath, cfg.SiteDomain)
+
+	routes.Setup(ctx, r, cfg, rend, healthcheck)
 
 	healthcheck.Start(ctx)
 
