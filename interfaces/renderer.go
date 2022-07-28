609276214eb58ac423bd9e20b1c9e701cea0f9bf
Jon Dewi Jones <jondewijones@gmail.com>
2022-07-28 16:16:55 +0100

    update interface for renderer and generate new mocks

--- interfaces/renderer.go
+++ interfaces/renderer.go
@@ -1,7 +1,14 @@
 package interfaces
 
+import (
+	"io"
+
+	coreModel "github.com/ONSdigital/dp-renderer/model"
+)
+
 //go:generate moq -out interfacestest/renderer.go -pkg interfacestest . Renderer
 
 type Renderer interface {
-	Do(path string, b []byte) ([]byte, error)
+	BuildPage(w io.Writer, pageModel interface{}, templateName string)
+	NewBasePageModel() coreModel.Page
 }
