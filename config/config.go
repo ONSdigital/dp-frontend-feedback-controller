62ec1900c5b21a7b87887f8b2b7c7b12b86efb64
Jon Dewi Jones <jondewijones@gmail.com>
2023-11-07 16:35:45 +0000

    update version of design system to e0a75c3

--- config/config.go
+++ config/config.go
@@ -44,7 +44,7 @@ func Get() (*Config, error) {
 	if envCfg.Debug {
 		envCfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		envCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/b28f101"
+		envCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/e0a75c3"
 	}
 	return envCfg, nil
 }
