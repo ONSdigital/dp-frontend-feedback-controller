aef0ce292adb8c7ff944d7ae63551230a20bc16d
Ighomena Odebala <ighomena.odebala@ons.gov.uk>
2025-03-07 09:38:43 +0000

    Update design system

--- config/config.go
+++ config/config.go
@@ -46,7 +46,7 @@ func Get() (*Config, error) {
 	if envCfg.Debug {
 		envCfg.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		envCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/2c5185a"
+		envCfg.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/6d9a222"
 	}
 
 	cfg = envCfg
